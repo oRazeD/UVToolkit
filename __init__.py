0dbc47a50df229998e617ed73c0c012570339819
ethan <ethan.simon.3d@gmail.com>
2023-02-16 18:36:06 -0500

    blender & add-on version bump, other small tweaks

--- __init__.py
+++ __init__.py
@@ -1,35 +1,16 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
-
 bl_info = {
     "name": "UV Toolkit",
     "author": "Alexander Belyakov",
-    "version": (2, 1, 0),
-    "blender": (2, 80, 0),
-    "location": "UV Editor",
-    "description": "UV editing tools",
-    "warning": "",
-    "wiki_url": "",
-    "category": "UV",
+    "version": (2, 1, 1),
+    "blender": (3, 4, 1),
+    "location": "UV Editor > Sidebar (N Panel)",
+    "description": "A collection of UV editing tools for Blender 2.8+",
+    "wiki_url": "https://alexbelyakov.gitlab.io/uv-toolkit-docs",
+    "tracker_url": "https://github.com/oRazeD/UVToolkit",
+    "category": "UV"
 }
 
 import bpy
-from bpy.props import PointerProperty
 
 from .operators.align_uv import AlignUv
 from .operators.border_seam import BorderSeam
@@ -195,7 +176,7 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
-    bpy.types.Scene.uv_toolkit = PointerProperty(type=UvToolkitProperties)
+    bpy.types.Scene.uv_toolkit = bpy.props.PointerProperty(type=UvToolkitProperties)
 
     wm = bpy.context.window_manager
 
@@ -226,3 +207,21 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
